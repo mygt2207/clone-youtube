df639a5f34aacb7e1136b27a0a910e22234d8a8b
Vorobiov Max <mycv2207@gmail.com>
2026-04-27 13:47:42 +0300

    test deploy

--- src/screen/NotFoundPage/NotFoundPage.tsx
+++ src/screen/NotFoundPage/NotFoundPage.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import React from 'react';
 
 export const NotFoundPage = () => {
+  console.log('logger', 'test deploy');
   return (
     <div className={s.container}>
       <Image
